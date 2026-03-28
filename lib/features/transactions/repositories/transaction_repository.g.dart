4d9d25ad1edad73f444c2d41e652dc121f9061bf
João Victor Panazzolo <joao.panazzolo@gmail.com>
2026-03-28 16:42:24 -0300

    feat: adding items to list

---
+++ lib/features/transactions/repositories/transaction_repository.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'transaction_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$transactionRepositoryHash() =>
+    r'59e181d8ce2cdf515b7fb1ee08b0c4f3003f5318';
+
+/// See also [TransactionRepository].
+@ProviderFor(TransactionRepository)
+final transactionRepositoryProvider =
+    AutoDisposeAsyncNotifierProvider<TransactionRepository, void>.internal(
+  TransactionRepository.new,
+  name: r'transactionRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$transactionRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$TransactionRepository = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
